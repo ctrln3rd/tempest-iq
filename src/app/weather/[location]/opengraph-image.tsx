83764bff12211929e51890342ce2c74db04db8c6
austinemark001 <markaustine001@gamil.com>
2025-02-22 15:57:24 +0300

    fixing font faimily typo

--- src/app/weather/[location]/opengraph-image.tsx
+++ src/app/weather/[location]/opengraph-image.tsx
@@ -1,6 +1,7 @@
 import { ImageResponse } from "next/og";
 
 export const runtime = 'edge';
+
 export async function GET({ params }: { params: { location: string } }) {
     const location = decodeURIComponent(params.location); 
 
