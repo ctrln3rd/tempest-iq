c389b447e28744ffd1afb95ce4fad231f15fe3fe
austinemark001 <markaustine001@gamil.com>
2025-03-21 15:05:06 +0300

    fixing .next error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ctrln3rd.weatherrush',
   appName: 'weather-rush',
-  webDir: 'public'
+  webDir: '.next'
 };
 
 export default config;
