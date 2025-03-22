f8169a9bf8df854f67989f964d469e080b36e8f0
austinemark001 <markaustine001@gamil.com>
2025-03-23 01:22:03 +0300

    editting offline page

--- src/app/offline/page.tsx
+++ src/app/offline/page.tsx
@@ -1,8 +1,8 @@
 export default function OfflinePage() {
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-2xl font-bold text-gray-800">🌐 You Are Offline</h1>
-        <p className="text-gray-600 mt-2">Check your internet connection and try again.</p>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-2xl font-bold">🌐 You Are Offline</h1>
+        <p className="mt-2">Check your internet connection and try again.</p>
       </div>
     );
   }
